6fa7d41b9d0eb94a6a397da189ddf0c5589031e9
Masakatsu Wawanabe <masakatsu@unslow.com>
2025-05-30 04:07:29 +0900

    feat(StateMachine): ステートマシンの構造を改善し、遷移定義を整理
    - Transitionクラスを追加し、イベントとステートの定義を明確化
    - TransitionMapクラスを実装し、遷移先の取得を簡素化
    - StateBehaviourクラスのコンストラクタを修正し、遷移マップの初期化を追加
    - StateMapクラスを追加し、ステートの管理を強化
    - Startメソッドを修正し、初期ステートの設定を明確化
    - SendEventメソッドを改善し、遷移処理を整理

--- UNSLOW/UnityUtils/Scripts/StateMachine.cs
+++ UNSLOW/UnityUtils/Scripts/StateMachine.cs
@@ -4,196 +4,214 @@ using System.Collections.Generic;
 namespace UNSLOW.UnityUtils
 {
     /// <summary>
-    /// 汎用のステートマシンクラス
-    /// 初期化済み辞書を用いることでセットアップコスト削減を図っている
+    ///     汎用のステートマシンクラス
+    ///     初期化済み辞書を用いることでセットアップコスト削減を図っている
+    ///     <typeparam name="TOwner">
+    ///         管理対象クラス
+    ///     </typeparam>
+    ///     <typeparam name="TState">
+    ///         enum継承のステート
+    ///     </typeparam>
+    ///     <typeparam name="TEvent">
+    ///         ステート遷移をトリガーするenum継承のイベント
+    ///     </typeparam>
     /// </summary>
-    public class StateMachine<TOwner, TState, TEvent> where TState : Enum where TEvent : Enum
+    public class StateMachine<TOwner, TState, TEvent>
+        where TState : Enum
+        where TEvent : Enum
     {
-        public record StateBehavior
+        /// <summary>
+        /// イベント別のステート遷移先定義
+        /// </summary>
+        public record Transition
         {
-            public Action<TOwner> OnEnter;
-            public Action<TOwner> OnUpdate;
-            public Action<TOwner> OnExit;
+            public readonly TEvent Event;
+            public readonly TState State;
 
-            public StateBehavior
-                (Action<TOwner> onEnter = null, Action<TOwner> onUpdate = null, Action<TOwner> onExit = null)
+            public Transition(TEvent ev, TState state)
             {
+                Event = ev;
+                State = state;
+            }
+        }
+
+        /// <summary>
+        ///     イベント遷移先の定義
+        /// </summary>
+        public class TransitionMap
+        {
+            private readonly Dictionary<TEvent, TState> _transitions;
+
+            public TransitionMap(params Transition[] transitions)
+            {
+                _transitions = new Dictionary<TEvent, TState>();
+                foreach (var transition in transitions)
+                    _transitions[transition.Event] = transition.State;
+            }
+            
+            public bool TryGetState(TEvent ev, out TState state)
+            {
+                return _transitions.TryGetValue(ev, out state);
+            }
+        }
+        
+        /// <summary>
+        ///     ステートの定義
+        ///     ステートイン、更新、ステートアウトの各コールバックと
+        ///     ステート依存のイベント遷移先を登録する
+        /// </summary>
+        public record StateBehaviour
+        {
+            public readonly TState State;
+            public readonly Action<TOwner> OnEnter;
+            public readonly Action<TOwner> OnUpdate;
+            public readonly Action<TOwner> OnExit;
+            private readonly TransitionMap _transitionMap;
+
+            /// <summary>
+            ///     コンストラクタ
+            /// </summary>
+            /// <param name="state"></param>
+            /// <param name="onEnter"></param>
+            /// <param name="onUpdate"></param>
+            /// <param name="onExit"></param>
+            /// <param name="transitions"></param>
+            public StateBehaviour
+            (   
+                TState state,
+                Action<TOwner> onEnter,
+                Action<TOwner> onUpdate,
+                Action<TOwner> onExit,
+                params Transition[] transitions)
+            {
+                State = state;
                 OnEnter = onEnter;
                 OnUpdate = onUpdate;
                 OnExit = onExit;
-            }
 
-            public StateBehavior WithEnter(Action<TOwner> action)
-            {
-                OnEnter = action;
-                return this;
+                if (transitions == null)
+                    return;
+                
+                // 必要であれば遷移先マップを作成
+                _transitionMap = new TransitionMap(transitions);
             }
-
-            public StateBehavior WithUpdate(Action<TOwner> action)
+            
+            /// <summary>
+            ///     イベントに対応した遷移先を得る
+            /// </summary>
+            /// <param name="ev"></param>
+            /// <param name="state"></param>
+            /// <returns></returns>
+            public bool TryGetNextState(TEvent ev, out TState state)
             {
-                OnUpdate = action;
-                return this;
+                return _transitionMap.TryGetState(ev, out state);
             }
-
-            public StateBehavior WithExit(Action<TOwner> action)
+        }
+        
+        /// <summary>
+        ///     ステートの定義マップ
+        /// </summary>
+        public class StateMap
+        {
+            private readonly Dictionary<TState, StateBehaviour> _stateMap;
+            
+            public StateMap(params StateBehaviour[] states)
             {
-                OnExit = action;
-                return this;
+                _stateMap = new Dictionary<TState, StateBehaviour>();
+                
+                foreach (var state in states)
+                    _stateMap[state.State] = state;
             }
-
-            public StateBehavior WithAll(Action<TOwner> enter, Action<TOwner> update, Action<TOwner> exit)
-            {
-                OnEnter = enter;
-                OnUpdate = update;
-                OnExit = exit;
-                return this;
-            }
-
-            public StateBehavior AddEnter(Action<TOwner> action)
-            {
-                OnEnter += action;
-                return this;
-            }
-
-            public StateBehavior AddUpdate(Action<TOwner> action)
-            {
-                OnUpdate += action;
-                return this;
-            }
-
-            public StateBehavior AddExit(Action<TOwner> action)
-            {
-                OnExit += action;
-                return this;
-            }
+            
+            public StateBehaviour this[TState state] => _stateMap[state];
         }
 
         public TState CurrentState { get; private set; }
 
         private TOwner _owner;
-        private StateBehavior _currentStateBehavior;
+        private StateBehaviour _currentStateBehaviour;
 
         /// <summary>
-        /// ステートのリスト
+        ///     ステートのマップ
         /// </summary>
-        private readonly Dictionary<TState, StateBehavior> _states;
+        private StateMap _stateMap;
 
         /// <summary>
-        /// イベント受信時の遷移先ステート辞書、現在のステート別で遷移先が変わる場合のもの
+        ///     ステートに依存しないイベント遷移先のマップ
         /// </summary>
-        private readonly Dictionary<(TEvent, TState), TState> _transitions;
+        private TransitionMap _transitionMapAny;
 
         /// <summary>
-        /// イベント受信時の遷移先ステート辞書
-        /// </summary>
-        private readonly Dictionary<TEvent, TState> _transitionsAny;
-
-        /// <summary>
-        /// ステートの辞書を作成する
-        /// </summary>
-        /// <param name="states"></param>
-        /// <returns></returns>
-        public static Dictionary<TState, StateBehavior> BuildStates(params (TState, StateBehavior)[] states)
-        {
-            var dict = new Dictionary<TState, StateBehavior>();
-            foreach (var (key, value) in states)
-                dict[key] = value;
-            return dict;
-        }
-
-        /// <summary>
-        /// 遷移元をキーとした遷移先の辞書を作成する
-        /// </summary>
-        /// <param name="transitions"></param>
-        /// <returns></returns>
-        public static Dictionary<(TEvent, TState), TState> BuildTransitions
-            (params ((TEvent, TState), TState)[] transitions)
-        {
-            var dict = new Dictionary<(TEvent, TState), TState>();
-            foreach (var ((ev, state), next) in transitions)
-                dict[(ev, state)] = next;
-            return dict;
-        }
-
-        /// <summary>
-        /// イベントと遷移先の対応辞書を作成する
-        /// </summary>
-        /// <param name="transitions"></param>
-        /// <returns></returns>
-        public static Dictionary<TEvent, TState> BuildTransitionsAny(params (TEvent, TState)[] transitions)
-        {
-            var dict = new Dictionary<TEvent, TState>();
-            foreach (var (ev, state) in transitions)
-                dict[ev] = state;
-            return dict;
-        }
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="states"></param>
-        /// <param name="transitions"></param>
-        /// <param name="transitionsAny"></param>
-        public StateMachine
-        (Dictionary<TState, StateBehavior> states,
-            Dictionary<(TEvent, TState), TState> transitions = null,
-            Dictionary<TEvent, TState> transitionsAny = null)
-        {
-            _states = states;
-            _transitions = transitions;
-            _transitionsAny = transitionsAny;
-        }
-
-        /// <summary>
-        /// ステートマシンの開始
+        ///     ステートマシンの開始
         /// </summary>
         /// <param name="owner"></param>
         /// <param name="initialState"></param>
-        public void Start(TOwner owner, TState initialState)
+        /// <param name="states"></param>
+        /// <param name="transitions"></param>
+        public void Start
+        (
+            TOwner owner,
+            TState initialState,
+            StateMap states,
+            TransitionMap transitions = null)
         {
             _owner = owner;
+            _stateMap = states;
+            _transitionMapAny = transitions;
+            
             CurrentState = initialState;
-
-            _currentStateBehavior = _states[CurrentState];
-            _currentStateBehavior.OnEnter?.Invoke(_owner);
-        }
-
-        public void SendEvent(TEvent ev)
-        {
-            if (_transitions != null &&
-                _transitions.TryGetValue((ev, CurrentState), out var next))
-            {
-                ChangeState(next);
-                return;
-            }
-
-            if (_transitionsAny == null)
-                throw new Exception("No transition found.");
-
-            ChangeState(_transitionsAny[ev]);
+            _currentStateBehaviour = _stateMap[CurrentState];
+            _currentStateBehaviour.OnEnter?.Invoke(owner);
         }
 
         /// <summary>
-        /// ステートを変更する
-        /// イベントの設定と無関係で動作するので注意
+        ///     イベント送信とそれに伴うステート遷移
+        /// </summary>
+        public bool SendEvent(TEvent ev)
+        {
+            if (_currentStateBehaviour == null)
+                throw new Exception("No states defined.");
+
+            // ステートに遷移先が登録されているのであれば遷移
+            if (_currentStateBehaviour.TryGetNextState(ev, out var state))
+            {
+                ChangeState(state);
+                return true;
+            }
+
+            // 無ければ遷移先マップを参照
+            if (_transitionMapAny != null &&
+                _transitionMapAny.TryGetState(ev, out var next))
+            {
+                ChangeState(next);
+                return true;
+            }
+
+            // どちらにも登録されていなければ無視
+            return false;
+        }
+
+        /// <summary>
+        ///     ステートを変更する
+        ///     イベントの設定と無関係で動作する
         /// </summary>
         /// <param name="newState"></param>
         public void ChangeState(TState newState)
         {
-            _currentStateBehavior.OnExit?.Invoke(_owner);
+            _currentStateBehaviour.OnExit?.Invoke(_owner);
 
             CurrentState = newState;
-            _currentStateBehavior = _states[CurrentState];
+            _currentStateBehaviour = _stateMap[CurrentState];
 
-            _currentStateBehavior.OnEnter?.Invoke(_owner);
+            _currentStateBehaviour.OnEnter?.Invoke(_owner);
         }
 
         /// <summary>
-        /// 更新
+        ///     更新
         /// </summary>
         public void Update()
         {
-            var curr = _states[CurrentState];
+            var curr = _stateMap[CurrentState];
             curr.OnUpdate?.Invoke(_owner);
         }
     }
