df3ac440f62a3c2261aa8bac53572521abe904d7
Masakatsu Wawanabe <masakatsu@unslow.com>
2025-07-27 17:39:26 +0900

    SingletonMonoBehabiourを全面更新

--- UNSLOW/UnityUtils/Scripts/SingletonMonoBehaviour.cs
+++ UNSLOW/UnityUtils/Scripts/SingletonMonoBehaviour.cs
@@ -3,28 +3,62 @@
 namespace UNSLOW.UnityUtils
 {
     /// <summary>
-    /// 最小のシングルトンなMonoBehaviour
-    /// MonoBehaviourのコールバックには一切干渉しない
+    ///     シングルトン MonoBehaviour
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    /// <typeparam name="T">シングルトン化するMonoBehaviour継承クラス</typeparam>
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
         public static T Instance { get; private set; }
 
-        protected SingletonMonoBehaviour()
+        /// <summary>
+        ///     Instanceの初期化
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void InitInstance() => Instance = null;
+        
+        /// <summary>
+        ///     Awakeでシングルトンの初期化を行う
+        ///     派生クラスで強引にAwake()を潰しまわないよう注意
+        /// </summary>
+        protected void Awake()
         {
-            if( Instance != null )
+            if (Instance && Instance != this)
             {
-                Destroy( this );
+                Destroy(gameObject);
                 return;
             }
 
             Instance = this as T;
+
+            OnSingletonAwake();
         }
 
-        protected void Revoke()
+        /// <summary>
+        ///     シングルトンの破棄を行う
+        ///     派生クラスで強引にOnDestroy()を潰しまわないよう注意
+        /// </summary>
+        protected void OnDestroy()
         {
+            if (Instance != this)
+                return;
+            
+            OnSingletonDestroy();
+            
             Instance = null;
         }
+
+        /// <summary>
+        ///     初期化メソッド
+        ///     SingletonMonoBehaviourがAwake()を使用しているため
+        ///     派生クラスではOnSingletonAwake()の実装を必須としている
+        /// </summary>
+        protected abstract void OnSingletonAwake();
+
+        /// <summary>
+        ///     破棄メソッド
+        ///     SingletonMonoBehaviourがOnDestroy()を使用しているため
+        ///     派生クラスではOnSingletonDestroy()の実装を必須としている
+        /// </summary>
+        protected abstract void OnSingletonDestroy();
     }
 }
